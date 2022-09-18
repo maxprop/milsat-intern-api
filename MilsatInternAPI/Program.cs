f4e3a651232e66d7618748422a5274826fa2a13f
Matthew Oke <matthewoke.ai@gmail.com>
2022-09-18 21:55:55 +0100

    simple-edits

--- MilsatInternAPI/Program.cs
+++ MilsatInternAPI/Program.cs
@@ -63,11 +63,13 @@ try
     var app = builder.Build();
 
     // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI(); 
-    }
+    //if (app.Environment.IsDevelopment())
+    //{
+    //    app.UseSwagger();
+    //    app.UseSwaggerUI(); 
+    //}
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
     app.UseHttpsRedirection();
 
